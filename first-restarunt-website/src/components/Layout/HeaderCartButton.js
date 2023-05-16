99a52600ab81e06f872315641c1ea3ea66b405da
saiashish8364 <polusaiashish97@gmail.com>
2023-05-16 17:56:25 +0530

    upload2

--- first-restarunt-website/src/components/Layout/HeaderCartButton.js
+++ first-restarunt-website/src/components/Layout/HeaderCartButton.js
@@ -6,7 +6,7 @@ const HeaderCartButton = (props) => {
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span>Cart</span>
       <span className={classes.badge}>0</span>
     </button>
   );
