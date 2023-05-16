69221ee93295266b10a39322d5c4e91a2573648d
saiashish8364 <polusaiashish97@gmail.com>
2023-05-16 12:32:29 +0530

    upload1

---
+++ first-restarunt-website/src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import restaruntImage from "../../assets/restarunt.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>React Restarunt</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={restaruntImage} alt="A table full of delicious food." />
+      </div>
+    </React.Fragment>
+  );
+};
+export default Header;
